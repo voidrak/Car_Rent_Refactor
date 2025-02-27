e887e1872d3fe2a9c52bd6a8a1f9b739bdd9f14d
RohaTech <raklovetocode@gmail.com>
2025-02-27 10:07:29 +0300

    Add role column to users table and implement car routes with CRUD actions

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,7 +10,19 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/users', UserController::class);
+});
 
+Route::get('/cars', [CarController::class, 'index'])->name('cars.index');
+Route::post('/cars', [CarController::class, 'store'])->name('cars.store');
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/cars/{car}', [CarController::class, 'show'])->name('cars.show');
+    Route::put('/cars/{car}', [CarController::class, 'update'])->name('cars.update');
+    Route::delete('/cars/{car}', [CarController::class, 'destroy'])->name('cars.destroy');
+});
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
